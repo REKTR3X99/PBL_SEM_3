2f32f363e48ba053cffc609723a4aee36a60bd7f
REKTR3X99 <ajaypnair99@gmail.com>
2018-08-17 17:06:55 +0530

    Added country list and GUI issues are fixed

--- src/Initializer.java
+++ src/Initializer.java
@@ -1,7 +1,8 @@
 //import java.util.*;
 import javax.swing.*;
 import java.awt.*;
-
+import java.util.ArrayList;
+import java.io.*;
 public class Initializer
 {
 	public static void main(String args[])
@@ -11,66 +12,50 @@ public class Initializer
 	}
 	
 	//Generates Window frame and adds the buttons and panels
-	void InitFrame()
+	void InitFrame() 
 	{
 		Dimension ButtonDimmension = new Dimension(100,100);
-		DefaultListModel<String> CountryListModel = new DefaultListModel<String>();
+		ArrayList<String> CountryList = new ArrayList<String>();
+		BufferedReader Reader = null; //BufferedReader Object
+		String line = "";
+		//Checking if file exists
+		try
+		{	
+			Reader = new BufferedReader(new FileReader("Resources/CountryNames.dat")); 	
+		}catch(FileNotFoundException e)
+		{
+			final  JPanel panel = new JPanel(); //JPanel for error
+			JOptionPane.showMessageDialog(panel,"could not open file", "Error", JOptionPane.ERROR_MESSAGE);  //Displaying error
+		}
 		
-		
-		/*
-		 * Add a itterative statement to pass valeus of country to DefaultListModel
-		 * */
+		while(line != null)
+		{
+			try {
+				line = Reader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			CountryList.add(line);
+		}
 		
 		
 		
 		JFrame Frame = new JFrame(); //BaseFrame Initialization
-		JPanel Panel = new JPanel();
-		JButton  PressButton = new JButton();
-		JList<String> CountryList = new JList<String>(CountryListModel);
-		JComboBox<String> CountriesComboBox = new JComboBox<String>();
-		DefaultComboBoxModel<String> CountryListDefModel = new DefaultComboBoxModel<String>();
-		
-		CountryList.setModel(CountryListDefModel);
-		CountriesComboBox.setModel(CountryListDefModel);
-		CountriesComboBox.add(CountryList);
+		JPanel Panel = new JPanel(); //Base Panel 
+		JButton  PressButton = new JButton(); //Button 
+		JComboBox<String> CountriesComboBox = new JComboBox<String>(); //Combo box
+		CountriesComboBox.setModel(new DefaultComboBoxModel(CountryList.toArray()));
 		
 		
-		Frame.add(Panel);
-		Panel.setLayout(new GridLayout(0,2,10,10));
-		Panel.add(CountriesComboBox);
-		Panel.add(PressButton);
+		Frame.add(Panel); //adding panel to the frame
+		Panel.setLayout(new GridLayout(0,2,10,10)); //setting the paneel layout
+		Panel.add(CountriesComboBox);//adding combo box to the panel
+		Panel.add(PressButton); //Adding button to the panel
 		
 		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Frame.setSize(600,480);
-		Frame.setVisible(true);
-		
-		
-		/*JFrame Frame = new JFrame(); //Base Frame Initialization
-		Dimension ButtonDim = new Dimension(100,100); //Dimension variable to set an uniform size to the buttons
-		DefaultListModel<String> CountryListModel = new DefaultListModel<String>(); //DefaultListModel to store the list of countries
-		JButton  Press = new JButton("Press"); //Initializing button with the text Press
-		JList <String>CountryList = new JList<String>(CountryListModel); //Initializing JList to add into the frame with the list "CountryListModel"
-		JPanel Panel = new JPanel();
-		
-		
-		CountryListModel.add(0, "India"); //Adding countries to the List
-		CountryListModel.add(1,"United Kingdom");
-		//Setting the base Frame
-		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Frame.setSize(640,480); //Window Size of 300x300 pixels
-	
-		//Setting panel Layout
-		Panel.setLayout(new GridLayout(0,2,10,10));
-		
-		//Adding panel to the frame
-		Frame.add(Panel); 
-		
-		
-		Panel.add(Press); //Adding the press button to the panel
-		Panel.add(CountryList); //Adding the country selection into the Drop Down menu
-		
-		Frame.setVisible(true);*/
-		
+		Frame.setVisible(true); 	
 	}
 	
 	//To get the display resolution and dynamically adjust the default value of the window
