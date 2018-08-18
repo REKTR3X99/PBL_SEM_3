8409f3806e4a7ede2b283079064802592b94520a
REKTR3X99 <ajaypnair99@gmail.com>
2018-08-19 01:20:08 +0530

    Fixed issue where capitals won't be displayed in the label

--- src/Initializer.java
+++ src/Initializer.java
@@ -10,10 +10,10 @@ public class Initializer
 	
 	JFrame Frame = new JFrame(); //BaseFrame Initialization
 	JPanel SelectionPanel = new JPanel(); //Selection Panel
-	JButton  PressButton = new JButton("Click"); //Button 
 	static JComboBox<String> CountriesComboBox = new JComboBox<String>(); //Combo box
 	static JLabel CapitalDisplay = new JLabel();
-	
+	static ArrayList<String> CountryList = new ArrayList<String>();
+	static ArrayList<String> CapitalList = new ArrayList<String>();
 	
 	public static void main(String args[])
 	{
@@ -24,8 +24,7 @@ public class Initializer
 	//Generates Window frame and adds the buttons and panels
 	void InitFrame() 
 	{
-		ArrayList<String> CountryList = new ArrayList<String>();
-		ArrayList<String> CapitalList = new ArrayList<String>();
+		
 		BufferedReader CountryListReader = null; //BufferedReader Object
 		BufferedReader CapitalListReader = null;
 		String CountryLine = "";
@@ -62,23 +61,21 @@ public class Initializer
 		
 		CountriesComboBox.setModel(new DefaultComboBoxModel(CountryList.toArray()));
 		CountriesComboBox.setBounds(10, 10, 500, 40);
-		
-		PressButton.setBounds(520,10,200,40);
+
 		
 		SelectionPanel.setLayout(null);
 		Frame.add(SelectionPanel); //adding panel to the frame
 		SelectionPanel.add(CountriesComboBox);//adding combo box to the panel
-		SelectionPanel.add(PressButton); //Adding button to the panel
 		
 		
+		//Boundaries and adding Label to SelectionPanel
 		CapitalDisplay.setBounds(10, 100, 600, 50);
 		SelectionPanel.add(CapitalDisplay);
 		
 		//Binding action listeners
-		PressButton.addActionListener(ActionListener.PressButtonAction);
 		CountriesComboBox.addActionListener(ActionListener.ComboBoxAction);
 		
-		CapitalDisplay.setText("World");
+		
 		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Frame.setSize(600,480);
 		Frame.setVisible(true); 	
--- src/Listener.java
+++ src/Listener.java
@@ -2,27 +2,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Listener
-{
-	
-	Initializer InitObj = new Initializer();
-	
+{	
 	ActionListener ComboBoxAction = new ActionListener()
 	{
-		
 		public void actionPerformed(ActionEvent e)
 		{
-			System.out.println("Hello");
+			
+			Initializer.CapitalDisplay.setText(Initializer.CapitalList.get(Initializer.CountriesComboBox.getSelectedIndex()));
 			
 		}
 	};
-
-	ActionListener PressButtonAction = new ActionListener()
-			{
-				
-				public void actionPerformed(ActionEvent e)
-				{
-					Initializer.CapitalDisplay.setText("Hello");
-					
-				}
-			};
 }
